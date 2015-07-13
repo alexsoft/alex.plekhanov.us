df8668217d48e3eaab4852d8ca18334343805085
Alexey Plekhanov <a.plekhanov@908.vc>
2015-07-13 18:11:23 +0200

    Improvements

--- resources/views/blog/post.blade.php
+++ resources/views/blog/post.blade.php
@@ -21,13 +21,13 @@
         <div class="col-sm-2">
             <div class="fb-like" data-href="{{ URL::current() }}" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false"></div>
         </div>
-        <div class="col-sm-2">
+        <div class="col-sm-3">
             <div id="vk_like"></div>
             <script type="text/javascript">
                 VK.Widgets.Like("vk_like", {type: "button", height: 22});
             </script>
         </div>
-        <div class="col-sm-2">
+        <div class="col-sm-3">
             <a data-pocket-label="pocket" data-pocket-count="horizontal" class="pocket-btn" data-lang="en"></a>
             <script type="text/javascript">!function(d,i){if(!d.getElementById(i)){var j=d.createElement("script");j.id=i;j.src="https://widgets.getpocket.com/v1/j/btn.js?v=1";var w=d.getElementById(i);d.body.appendChild(j);}}(document,"pocket-btn-js");</script>
         </div>
