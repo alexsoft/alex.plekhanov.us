33a3f46e1f7ac0085b323eb6ebacaa424053465b
Alexey Plekhanov <a.plekhanov@908.vc>
2015-07-13 17:51:52 +0200

    Add FB like button

--- resources/views/_layouts/default.blade.php
+++ resources/views/_layouts/default.blade.php
@@ -1,68 +1,88 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<meta name="theme-color" content="#eeeeee">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <meta charset="utf-8">
+        <meta name="theme-color" content="#eeeeee">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-		<meta name="description" content="Web Artisan, Developer, PHP Developer, Web Developer, JS Developer, Laravel Developer, Laravel, Javascript, JS, Javascript Developer, PHP">
-		<meta name="author" content="Alexey Plekhanov">
-		<link rel="icon" href="{{ asset('ico/favicon.ico') }}">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <meta name="description" content="Web Artisan, Developer, PHP Developer, Web Developer, JS Developer, Laravel Developer, Laravel, Javascript, JS, Javascript Developer, PHP">
+        <meta name="author" content="Alexey Plekhanov">
+        <link rel="icon" href="{{ asset('ico/favicon.ico') }}">
 
-		<meta name="google-site-verification" content="dW-i1q_ReYQ2XTd7hwnMo7J-ER6A33yAScHyxw9Zqu4" />
-		<meta name='yandex-verification' content='625e2fdaac325346' />
+        <meta name="google-site-verification" content="dW-i1q_ReYQ2XTd7hwnMo7J-ER6A33yAScHyxw9Zqu4" />
+        <meta name='yandex-verification' content='625e2fdaac325346' />
 
-		<title>@yield('page_title', 'Alex.Plekhanov.us')</title>
-		@section('css')
-			<link href="{{ asset('css/app.css') }}" rel="stylesheet">
-		@show
+        <title>@yield('page_title', 'Alex.Plekhanov.us')</title>
+        @section('css')
+            <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        @show
 
-		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-		<!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-		<![endif]-->
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
 
-		<link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{ asset('ico/touch144.png') }}">
-		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{ asset('ico/touch114.png') }}">
-		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{ asset('ico/touch72.png') }}">
-		<link rel="apple-touch-icon-precomposed" href="{{ asset('ico/touch57.png') }}">
-	</head>
-	<body class="{{ (Route::is('pages.cv')) ? 'cv' : '' }}">
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{ asset('ico/touch144.png') }}">
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{ asset('ico/touch114.png') }}">
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{ asset('ico/touch72.png') }}">
+        <link rel="apple-touch-icon-precomposed" href="{{ asset('ico/touch57.png') }}">
 
-		@include('_partials.nav')
+        @yield('meta')
+    </head>
+    <body class="{{ (Route::is('pages.cv')) ? 'cv' : '' }}">
+        @if(Route::is('blog.post'))
+            <script>
+                window.fbAsyncInit = function() {
+                    FB.init({
+                        appId      : '1015546005136408',
+                        xfbml      : true,
+                        version    : 'v2.4'
+                    });
+                };
+                (function(d, s, id){
+                    var js, fjs = d.getElementsByTagName(s)[0];
+                    if (d.getElementById(id)) {return;}
+                    js = d.createElement(s); js.id = id;
+                    js.src = "//connect.facebook.net/en_US/sdk.js";
+                    fjs.parentNode.insertBefore(js, fjs);
+                }(document, 'script', 'facebook-jssdk'));
+            </script>
+        @endif
 
-		<div class="container">
-			<div class="row">
-				<div class="col-sm-12">
+        @include('_partials.nav')
 
-					<div class="row">
-						<div class="col-lg-8 col-md-10">
-							@yield('content')
-						</div>
-						@unless(Route::is('home'))
-							@include('_partials.aside')
-						@endunless
-					</div>
-				</div>
-			</div>
-		</div>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
 
-		@include('_partials.footer')
+                    <div class="row">
+                        <div class="col-lg-8 col-md-10">
+                            @yield('content')
+                        </div>
+                        @unless(Route::is('home'))
+                            @include('_partials.aside')
+                        @endunless
+                    </div>
+                </div>
+            </div>
+        </div>
 
-		@section('js')
-			<script src="{{ asset('js/all.js') }}" async></script>
-		@show
-		<script>
-			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-					m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-			ga('create', 'UA-41504409-1', 'auto');
-			ga('send', 'pageview');
-		</script>
-		<script type="text/javascript"> (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter31059421 = new Ya.Metrika({ id:31059421, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");</script><noscript><div><img src="https://mc.yandex.ru/watch/31059421" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-	</body>
+        @include('_partials.footer')
+
+        @section('js')
+            <script src="{{ asset('js/all.js') }}" async></script>
+        @show
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+            ga('create', 'UA-41504409-1', 'auto');
+            ga('send', 'pageview');
+        </script>
+        <script type="text/javascript"> (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter31059421 = new Ya.Metrika({ id:31059421, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");</script><noscript><div><img src="https://mc.yandex.ru/watch/31059421" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    </body>
 </html>
--- resources/views/blog/post.blade.php
+++ resources/views/blog/post.blade.php
@@ -1,5 +1,12 @@
 @extends('_layouts.default')
 
+@section('meta')
+    <meta property="og:title" content="«{{ $title }}» by Alexey Plekhanov" />
+    <meta property="og:url" content="{{ URL::current() }}" />
+    <meta property="og:type" content="post" />
+    <meta property="article:author" content="https://www.facebook.com/alex.plekhanov" />
+@endsection
+
 @section('content')
 
     <h1>{{ $title }}</h1>
@@ -7,6 +14,12 @@
     <div class="row">
         <div class="col-sm-12 text-justify">
             {!! $text !!}
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="fb-like" data-href="{{ URL::current() }}" data-layout="button" data-action="like" data-show-faces="false" data-share="false"></div>
         </div>
     </div>
 
