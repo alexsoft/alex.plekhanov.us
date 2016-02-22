a0744055dde9b8342d41e722a3d8d4321b9b0c9b
Alexey Plekhanov <a.plekhanov@908.vc>
2016-02-22 14:46:45 +0100

    Rename laravel logo file to affect browser cache

--- resources/views/_partials/footer.blade.php
+++ resources/views/_partials/footer.blade.php
@@ -11,7 +11,7 @@
                 <div class="col-sm-6 text-right">
                     <p class="technologies">
                         <img src="{{ asset('img/php.png') }}" alt="PHP" title="PHP" width="50" />
-                        <img src="{{ asset('img/laravel.png') }}" alt="Laravel" title="Laravel" width="50" />
+                        <img src="{{ asset('img/laravel5.png') }}" alt="Laravel" title="Laravel" width="50" />
                         <img src="{{ asset('img/js.png') }}" alt="JS" title="JS" width="50" />
                         <img src="{{ asset('img/html5.png') }}" alt="HTML 5" title="HTML 5" width="50" />
                     </p>
