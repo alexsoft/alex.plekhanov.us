86793f4fcabcaa03dbc7c49b71ccdf700017fd3b
Alexey Plekhanov <a.plekhanov@908.vc>
2016-09-09 12:13:27 +0200

    Update footer styling, add deploykit link

--- resources/views/_partials/footer.blade.php
+++ resources/views/_partials/footer.blade.php
@@ -1,23 +1,30 @@
 <footer>
-
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-6">
-                <p>
-                    © {{ date('Y') }} Alex Plekhanov
-                </p>
-            </div>
-            @unless(Route::is('pages.cv'))
-                <div class="col-sm-6 text-right">
-                    <p class="technologies">
-                        <img src="{{ asset('img/php.png') }}" alt="PHP" title="PHP" width="50" />
-                        <img src="{{ asset('img/laravel5.png') }}" alt="Laravel" title="Laravel" width="50" />
-                        <img src="{{ asset('img/js.png') }}" alt="JS" title="JS" width="50" />
-                        <img src="{{ asset('img/html5.png') }}" alt="HTML 5" title="HTML 5" width="50" />
+    <div class="container" style="height: 60px;">
+        <div class="col-sm-6 copyright">
+            <div class="row">
+                <div class="col-sm-12">
+                    <p>
+                        © {{ date('Y') }} Alex Plekhanov
                     </p>
                 </div>
-            @endunless
+            </div>
+            <div class="row">
+                <div class="col-sm-12">
+                    <p style="font-size: 75%;">
+                        <a href="https://deploykit.co" target="_blank">
+                            Deploy PHP apps with a single click
+                        </a>
+                    </p>
+                </div>
+            </div>
         </div>
+        @unless(Route::is('pages.cv'))
+            <div class="col-sm-6 techs">
+                <img src="{{ asset('img/php.png') }}" alt="PHP" title="PHP" width="50" />
+                <img src="{{ asset('img/laravel5.png') }}" alt="Laravel" title="Laravel" width="50" />
+                <img src="{{ asset('img/js.png') }}" alt="JS" title="JS" width="50" />
+                <img src="{{ asset('img/html5.png') }}" alt="HTML 5" title="HTML 5" width="50" />
+            </div>
+        @endunless
     </div>
-
 </footer>
