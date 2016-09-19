f0a414cb0901338b99cd8a410f7bbe78078d3853
Alexey Plekhanov <a.plekhanov@908.vc>
2016-09-19 10:51:21 +0200

    Update dates in sitemap

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -14,19 +14,19 @@ class PagesController extends Controller
         $pages = [
             [
                 'loc'        => '/',
-                'lastmod'    => '2015-11-02',
+                'lastmod'    => '2016-09-18',
                 'changefreq' => 'monthly',
                 'priority'   => '0.7',
             ],
             [
                 'loc'        => 'cv',
-                'lastmod'    => '2016-04-12',
+                'lastmod'    => '2016-09-18',
                 'changefreq' => 'monthly',
                 'priority'   => '0.9',
             ],
             [
                 'loc'        => 'blog',
-                'lastmod'    => '2016-03-01',
+                'lastmod'    => '2016-09-18',
                 'changefreq' => 'monthly',
                 'priority'   => '0.5',
             ],
@@ -34,37 +34,37 @@ class PagesController extends Controller
                 'loc'        => 'blog/2014/07/php-user-group',
                 'lastmod'    => '2014-07-18',
                 'changefreq' => 'yearly',
-                'priority'   => '0.5',
+                'priority'   => '0.3',
             ],
             [
                 'loc'        => 'blog/2014/12/it-talk-php-odessa',
                 'lastmod'    => '2014-12-04',
                 'changefreq' => 'yearly',
-                'priority'   => '0.5',
+                'priority'   => '0.3',
             ],
             [
                 'loc'        => 'blog/2014/12/php-user-group-3',
                 'lastmod'    => '2014-12-18',
                 'changefreq' => 'yearly',
-                'priority'   => '0.5',
+                'priority'   => '0.3',
             ],
             [
                 'loc'        => 'blog/2015/07/blog-v3',
                 'lastmod'    => '2015-07-13',
                 'changefreq' => 'yearly',
-                'priority'   => '0.5',
+                'priority'   => '0.3',
             ],
             [
                 'loc'        => 'blog/2015/09/php-question',
                 'lastmod'    => '2015-09-30',
                 'changefreq' => 'yearly',
-                'priority'   => '0.5',
+                'priority'   => '0.3',
             ],
             [
                 'loc'        => 'blog/2016/03/lets-get-encrypted',
                 'lastmod'    => '2016-03-01',
                 'changefreq' => 'yearly',
-                'priority'   => '0.6',
+                'priority'   => '0.5',
             ],
         ];
 
