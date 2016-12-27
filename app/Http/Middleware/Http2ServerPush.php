9318b9d967b0e14295a29ddd6ade3e220a5e5a00
Alexey Plekhanov <a.plekhanov@908.vc>
2016-12-27 15:04:14 +0100

    Add middleware for probably future http2 server push

---
+++ app/Http/Middleware/Http2ServerPush.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Alex\Http\Middleware;
+
+use Closure;
+
+class Http2ServerPush
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $response =  $next($request);
+
+        $images = ['/img/alexey@2x.jpg', '/img/php.png', '/img/laravel5.png', '/img/js.png', '/img/html5.png'];
+
+//        Link: <https://example.com/other/styles.css>; rel=preload; as=style
+        foreach ($images as $image) {
+            $response->headers->set('Link',  '<' . asset($image) . '>; rel=preload; as=image', false);
+        }
+
+        return $response;
+    }
+}
