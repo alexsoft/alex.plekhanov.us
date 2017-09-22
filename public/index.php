fd23f0718fbd7d40ceb517eb17629cb147857e74
Alexey Plekhanov <alexsoft@users.noreply.github.com>
2017-09-22 17:12:49 +0300

    Laravel 5.5

--- public/index.php
+++ public/index.php
@@ -5,6 +5,8 @@
  * @author   Taylor Otwell <taylorotwell@gmail.com>
  */
 
+define('LARAVEL_START', microtime(true));
+
 /*
 |--------------------------------------------------------------------------
 | Register The Auto Loader
@@ -17,7 +19,7 @@
 |
 */
 
-require __DIR__.'/../bootstrap/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
