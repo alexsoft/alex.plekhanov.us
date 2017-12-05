df201b399b7623850b3b566c90543259a626054f
Alexey Plekhanov <alexsoft@users.noreply.github.com>
2017-12-05 16:35:57 +0100

    Update bulma.blade.php

--- resources/views/_layouts/bulma.blade.php
+++ resources/views/_layouts/bulma.blade.php
@@ -9,7 +9,6 @@
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <meta name="description" content="@yield('meta_description', 'Alexey Plekhanov is a developer on highload project and owner at Deploy Kit.')">
     <meta name="author" content="Alexey Plekhanov">
-    <meta name="telegram:channel" content="@alexdeveloper">
     <meta property="fb:pages" content="704409203090496" />
 
     @stack('meta')
