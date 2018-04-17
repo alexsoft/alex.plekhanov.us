8e42d8a23b90ed95e6cfa450283fa73382a8386c
Alex Plekhanov <alex@plekhanov.us>
2018-04-17 23:04:12 +0200

    Updated theme-color

--- resources/views/_layouts/default.blade.php
+++ resources/views/_layouts/default.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <meta name="theme-color" content="#eeeeee">
+        <meta name="theme-color" content="#363636">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
