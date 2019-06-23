baadac99f71b14ac16c49095db083546fae2452f
Alex Plekhanov <oleksii.plekhanov@gmail.com>
2019-06-23 20:38:22 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,17 @@
 
 use Alex\Http\Controllers\PagesController;
 use Alex\Http\Controllers\PostsController;
-use Alex\Http\Controllers\ProjectsController;
 use Alex\Http\Controllers\TravelController;
+use Alex\Http\Controllers\ProjectsController;
 
 Route::view('/', 'welcome')->name('home');
 
 Route::get('/cv', [PagesController::class, 'cv'])->name('pages.cv');
 
 Route::group(['prefix' => 'blog'], function () {
-
     Route::get('/', [PostsController::class, 'index'])->name('blog.index');
 
     Route::get('{year}/{month}/{title}', [PostsController::class, 'show'])->name('blog.post');
-
 });
 
 Route::group(['prefix' => 'projects', 'as' => 'projects.'], function () {
