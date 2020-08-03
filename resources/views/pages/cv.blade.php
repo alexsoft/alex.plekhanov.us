a8db218e1e8b9dc46722eede677303fb00bf9252
Alex Plekhanov <alexsoft@users.noreply.github.com>
2020-08-03 11:30:32 +0200

    Update cv.blade.php

--- resources/views/pages/cv.blade.php
+++ resources/views/pages/cv.blade.php
@@ -19,7 +19,7 @@
             <div class="column">
                 <div class="content">
                     <h1 class="accent"><span itemprop="givenName">Alex</span> <span itemprop="familyName">Plekhanov</span></h1>
-                    <h2>PHP Developer</h2>
+                    <h2>Software Engineer, PHP</h2>
                 </div>
             </div>
         </div>
