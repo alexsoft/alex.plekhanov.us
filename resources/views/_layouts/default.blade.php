9227ffce6b5a089f0d3712d95ab5be8408f0c913
Alex Plekhanov <alex@plekhanov.io>
2022-06-14 23:23:43 +0200

    Removed old favicon tag

--- resources/views/_layouts/default.blade.php
+++ resources/views/_layouts/default.blade.php
@@ -11,7 +11,6 @@
             <meta name="description" content="@yield('meta_description', 'Alex Plekhanov is a Web developer who is always in process of doing something great')">
             <meta name="author" content="Alex Plekhanov">
         @show
-        <link rel="icon" href="{{ asset('ico/favicon.ico') }}">
 
         <meta property="og:url" content="{{ request()->url() }}" />
         <meta property="og:title" content="@yield('og_title')" />
