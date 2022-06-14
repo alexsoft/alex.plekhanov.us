5d568a49246de9216d35fab48be99e91f7425866
Alex Plekhanov <alex@plekhanov.io>
2022-06-14 23:00:37 +0200

    Tightened code of Country entity

--- app/Entities/Country.php
+++ app/Entities/Country.php
@@ -1,35 +1,24 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Alex\Entities;
 
 use Stidges\CountryFlags\CountryFlag;
 
 class Country
 {
-    /**
-     * @var string
-     */
-    protected $name;
+    private string $name;
+
+    private string $code;
+
+    private string $flag;
+
+    /** @var string[] */
+    private array $cities;
 
     /**
-     * @var string
-     */
-    protected $code;
-
-    /**
-     * @var string
-     */
-    protected $flag;
-
-    /**
-     * @var array
-     */
-    protected $cities;
-
-    /**
-     * @param string $name
-     * @param string $code
-     * @param array $cities
+     * @param string[] $cities
      */
     public function __construct(string $name, string $code, array $cities)
     {
@@ -39,41 +28,29 @@ class Country
         $this->cities = $cities;
     }
 
-    /**
-     * @return string
-     */
     public function getName(): string
     {
         return $this->name;
     }
 
-    /**
-     * @return string
-     */
     public function getCode(): string
     {
         return $this->code;
     }
 
-    /**
-     * @return string
-     */
     public function getFlag(): string
     {
         return $this->flag;
     }
 
     /**
-     * @return array
+     * @return string[]
      */
     public function getCities(): array
     {
         return $this->cities;
     }
 
-    /**
-     * @return string
-     */
     public function getCitiesString(): string
     {
         return implode(', ', $this->getCities());
